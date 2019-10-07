3a729dff2e50d5df2a3155558007261ec2282e42
Chris Wilkinson <chris@chriswilkinson.co>
2019-10-08 09:32:00 +1000

    hsl to rgb, better colours

--- dist/d3-flamegraph.js
+++ dist/d3-flamegraph.js
@@ -5007,13 +5007,37 @@ var flamegraph = function () {
     return hash
   } */
 
+  function hslToRgb (h, s, l) {
+    var r, g, b;
+
+    if (s === 0) {
+      r = g = b = l;
+    } else {
+      var hue2rgb = function hue2rgb (p, q, t) {
+        if (t < 0) t += 1;
+        if (t > 1) t -= 1;
+        if (t < 1 / 6) return p + (q - p) * 6 * t
+        if (t < 1 / 2) return q
+        if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6
+        return p
+      };
+
+      var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+      var p = 2 * l - q;
+      r = hue2rgb(p, q, h + 1 / 3);
+      g = hue2rgb(p, q, h);
+      b = hue2rgb(p, q, h - 1 / 3);
+    }
+
+    return [Math.round(r * 255), Math.round(g * 255), Math.round(b * 255)]
+  }
+
   function colorLeaves (leaves) {
     if (leaves == null) return 'rgb(240,240,240)'
-    let vector = 1 - (1 / Math.cbrt(leaves + 1));
-    let r = 150 + Math.round(100 * vector);
-    let g = 120 + Math.round(105 * (1 - vector));
-    let b = 70 + Math.round(165 * (1 - vector));
-    return 'rgb(' + r + ',' + g + ',' + b + ')'
+    let variance = 3;
+    let vector = (1 / Math.sqrt(leaves + 1)) / variance;
+    var hslrgb = hslToRgb(vector, 0.7, 0.7);
+    return 'rgb(' + hslrgb[0] + ',' + hslrgb[1] + ',' + hslrgb[2] + ')'
   }
 
   /* function colorHash (name, libtype, delta) {
